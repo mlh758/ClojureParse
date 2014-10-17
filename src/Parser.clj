4fa232a25d5ebd2006eff42d4f6bf45a9c5a1f96
Michael <osiris2918@gmail.com>
2014-10-16 21:49:03 -0500

    Meeting Specification
    Program currently parses the type of strings expected for the project.
    This may be subject to change, further testing needed.

--- src/Parser.clj
+++ src/Parser.clj
@@ -1,25 +1,31 @@
 (require '[clojure.string :as str])
 (defn split-line [line]
+  "Splits a line of text into a sequence of strings"
   (str/split line #" "))
 
 (defn read-lines [filename]
+  "Reads a file and returns a sequence made up of a vector for each line"
   (with-open [reader (clojure.java.io/reader filename)]
     (for [line (doall (line-seq reader))] (split-line line))))
 
-;(doseq [line (read-lines "C:\\Users\\osiri_000\\Documents\\GitHub\\ClojureParse\\commands.txt")]
-;  (println line))
-
 (defn id? [check]
+  "Checks if CHECK is an ID"
   (not (clojure.string/blank? (re-matches #"[A-Z]" check))))
 (defn number-char? [check]
+  "Checks if CHECK is a number character"
   (not (clojure.string/blank? (re-matches #"[0-9]" check))))
 (defn error []
   println "Reject")
 (defn accept []
   println "Accept")
+(defn valid-operator? [lexeme]
+  (cond
+    (= lexeme "+") true
+    :else false))
 
 (declare factor? expression? term?)
 (defn expression? [lexemes state]
+  "Verifies if input is an expression, updates STATE with FROM"
   (if (empty? lexemes)
     (if (= (:paren-depth state) 0)
       #(accept) ;then
@@ -28,18 +34,21 @@
   )
 
 (defn term? [lexemes state]
+  "Verifies if input is a term, updates STATE with FROM"
   (let [from (:from state)]
     (if (= from "factor")
       (if (empty? lexemes)
         #(expression? lexemes (assoc state :from "term"))
         (let [lexeme (first lexemes)]
           (cond
-            (= lexeme "+") #(factor? (rest lexemes) (assoc state :from "term"))
+            (valid-operator? lexeme) #(factor? (rest lexemes) (assoc state :from "term"))
             (= lexeme ")") #(factor? lexemes (assoc state :from "term"))
             :else #(error))))
       #(factor? lexemes (assoc state :from "term")))))
 
+
 (defn factor? [lexemes state]
+  "Checks if input is a factor, updates STATE with FROM and PAREN-DEPTH"
   (if (empty? lexemes)
     #(error)
     (let [lexeme (first lexemes) paren-depth (:paren-depth state)]
@@ -52,16 +61,18 @@
 
 
 (defn parse [input]
+  "Takes a vector of strings and either accepts or rejects it"
   (let [state {:paren-depth 0 :from nil} ]
     (println (trampoline expression? input state))))
 
 (defn parse-file [filename]
+  "Reads a file and parses each line"
   (let [lines (read-lines filename)]
     (doseq [line lines]
       (parse line))))
 
 (parse-file "C:\\Users\\osiri_000\\Documents\\GitHub\\ClojureParse\\commands.txt")
-;(parse ["A" "+" "(" "C" ")"])
+;(parse ["A" "+" "(" "C" ")" "+" "(" "1" "+" "5" ")" ])
 
 
 
