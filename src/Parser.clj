4c74784dbef15cf43c0102ff47ae88bf054cba72
Michael <osiris2918@gmail.com>
2014-10-17 22:30:59 -0500

    Improved Design
    Operator validation is now a distinct function for extensibility.
    
    Improved function names to be more idiomatic and less misleading.
    
    TODO: Refactor code to reduce dependence on state.
    Thank you /r/clojure for the generous feedback.

--- src/Parser.clj
+++ src/Parser.clj
@@ -1,79 +1,64 @@
-(require '[clojure.string :as str])
+(require '[clojure.string :as string])
 (defn split-line [line]
-  "Splits a line of text into a sequence of strings"
-  (str/split line #" "))
+  (string/split line #" "))
 
 (defn read-lines [filename]
-  "Reads a file and returns a sequence made up of a vector for each line"
   (with-open [reader (clojure.java.io/reader filename)]
-    (for [line (doall (line-seq reader))] (split-line line))))
+    (doall (map split-line (line-seq reader)))))
+
+
 
 (defn id? [check]
-  "Checks if CHECK is an ID"
-  (not (clojure.string/blank? (re-matches #"[A-Z]" check))))
+  (not (string/blank? (re-matches #"[A-Z]" check))))
 (defn number-char? [check]
-  "Checks if CHECK is a number character"
-  (not (clojure.string/blank? (re-matches #"[0-9]" check))))
+  (not (string/blank? (re-matches #"[0-9]" check))))
+(defn valid-operator? [operator]
+  (some #(= % operator) ["+"]))
 (defn error []
   println "Reject")
 (defn accept []
   println "Accept")
-(defn valid-operator? [lexeme]
-  (cond
-    (= lexeme "+") true
-    :else false))
 
-(declare factor? expression? term?)
-(defn expression? [lexemes state]
-  "Verifies if input is an expression, updates STATE with FROM"
+(declare validate-factor validate-expression validate-term)
+(defn validate-expression [lexemes state]
   (if (empty? lexemes)
     (if (= (:paren-depth state) 0)
       #(accept) ;then
       #(error)) ;else
-    #(term? lexemes (assoc state :from "expression"))) ;outer else
+    #(validate-term lexemes (assoc state :from "expression"))) ;outer else
   )
 
-(defn term? [lexemes state]
-  "Verifies if input is a term, updates STATE with FROM"
+(defn validate-term [lexemes state]
   (let [from (:from state)]
     (if (= from "factor")
       (if (empty? lexemes)
-        #(expression? lexemes (assoc state :from "term"))
+        #(validate-expression lexemes (assoc state :from "term"))
         (let [lexeme (first lexemes)]
           (cond
-            (valid-operator? lexeme) #(factor? (rest lexemes) (assoc state :from "term"))
-            (= lexeme ")") #(factor? lexemes (assoc state :from "term"))
+            (valid-operator? lexeme) #(validate-factor (rest lexemes) (assoc state :from "term"))
+            (= lexeme ")") #(validate-factor lexemes (assoc state :from "term"))
             :else #(error))))
-      #(factor? lexemes (assoc state :from "term")))))
+      #(validate-factor lexemes (assoc state :from "term")))))
 
-
-(defn factor? [lexemes state]
-  "Checks if input is a factor, updates STATE with FROM and PAREN-DEPTH"
+(defn validate-factor [lexemes state]
   (if (empty? lexemes)
     #(error)
     (let [lexeme (first lexemes) paren-depth (:paren-depth state)]
       (cond
-        (id? lexeme) #(term? (rest lexemes) (assoc state :paren-depth paren-depth :from "factor"))
-        (number-char? lexeme) #(term? (rest lexemes) (assoc state :paren-depth paren-depth :from "factor"))
-        (= lexeme "(") #(expression? (rest lexemes) (assoc state :paren-depth (inc paren-depth) :from "factor"))
-        (= lexeme ")") #(term? (rest lexemes) (assoc state :paren-depth (dec paren-depth) :from "factor"))
+        (id? lexeme) #(validate-term (rest lexemes) (assoc state :paren-depth paren-depth :from "factor"))
+        (number-char? lexeme) #(validate-term (rest lexemes) (assoc state :paren-depth paren-depth :from "factor"))
+        (= lexeme "(") #(validate-expression (rest lexemes) (assoc state :paren-depth (inc paren-depth) :from "factor"))
+        (= lexeme ")") #(validate-term (rest lexemes) (assoc state :paren-depth (dec paren-depth) :from "factor"))
         :else #(error)))))
 
 
 (defn parse [input]
-  "Takes a vector of strings and either accepts or rejects it"
   (let [state {:paren-depth 0 :from nil} ]
-    (println (trampoline expression? input state))))
+    (println (trampoline validate-expression input state))))
 
 (defn parse-file [filename]
-  "Reads a file and parses each line"
   (let [lines (read-lines filename)]
     (doseq [line lines]
       (parse line))))
 
 (parse-file "C:\\Users\\osiri_000\\Documents\\GitHub\\ClojureParse\\commands.txt")
-;(parse ["A" "+" "(" "C" ")" "+" "(" "1" "+" "5" ")" ])
-
-
-
-
